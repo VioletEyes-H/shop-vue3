c7ec22aec4ee56eeeb7ab11bcd845c35aa0f85cf
huangjun <1602599167@qq.com>
2024-12-28 20:18:00 +0800

    优化文件命名

---
+++ src/pages/home/Index.vue
@@ -0,0 +1,13 @@
+<template>
+  <view>
+    <custom-nav title="首页"/>
+  </view>
+</template>
+
+<script setup lang="ts">
+import CustomNav from "@/components/custom-nav.vue";
+
+</script>
+
+<style scoped>
+</style>
